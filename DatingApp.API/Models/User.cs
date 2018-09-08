616bdcba85bad84c560ebbe9f4da357db56e3095
Iuliia Aleksandrova <iualeksa@microsoft.com>
2018-09-07 17:50:59 -0700

    Added userModel

---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
